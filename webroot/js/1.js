fdee29d8fcafbc4c02d812ae35b70177ced0f7c1
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2025-11-14 12:23:54 +0100

    fix - clock now with images

--- webroot/js/1.js
+++ webroot/js/1.js
@@ -6,21 +6,75 @@ if(navigator.onLine==false){
     mensaje.style.display="block";
 }
 
+
+function rellenarTablaPuntuacion(puntuacion) {
+    let tabla=document.getElementById("puntuaciones");
+    
+
+
+    if(localStorage.getItem("puntuacion1")>puntuacion || tabla.children[1]=="Sin tiempo"){
+        let puntuacionSuperada=tabla.children[1];
+        tabla.children[1].innerHTML=puntuacion;
+        localStorage.setItem("puntuacion1",puntuacion);
+
+        if(localStorage.getItem("puntuacion1")>puntuacion){
+            tabla.children[2].innerHTML=puntuacionSuperada;
+            localStorage.setItem("puntuacion2",puntuacionSuperada);
+        }
+        
+    }else{
+        if(localStorage.getItem("puntuacion2")>puntuacion || tabla.children[2]=="Sin tiempo"){
+        let puntuacionSuperada=tabla.children[2];
+        tabla.children[2].innerHTML=puntuacion;
+        localStorage.setItem("puntuacion2",puntuacion);
+        
+        if(localStorage.getItem("puntuacion2")>puntuacion){
+            tabla.children[3].innerHTML=puntuacionSuperada;
+            localStorage.setItem("puntuacion3",puntuacionSuperada);
+        }
+        }else{
+            if(localStorage.getItem("puntuacion3")>puntuacion || tabla.children[3]=="Sin tiempo"){
+            tabla.children[3].innerHTML=puntuacion;
+            localStorage.setItem("puntuacion3",puntuacion);
+            }
+        }
+    }
+}
+
 function Reloj(){
+   let contador=0; 
    let actual=new Date();
    let horas=actual.getHours();
    let minutos=actual.getMinutes();
    let segundos=actual.getSeconds();
-   let hora="";
-   //Convertimos los números de un dígito a dos dígitos
-   horas = (horas<=9)?("0"+horas):horas;
-   minutos = (minutos<=9)?("0"+minutos):minutos;
-   segundos=(segundos<=9)? ("0"+segundos):segundos;        
+   let aDigitos=[];
+   //Convertimos los números de un dígito a dos dígitos y los metemos en un array
+   let digito1hora=parseInt(horas/10);
+   aDigitos.push(digito1hora);
+   let digito2hora=horas%10;
+   aDigitos.push(digito2hora);
 
-   hora = horas+":"+minutos+":" +segundos
+   let digito1minuto=parseInt(minutos/10);
+   aDigitos.push(digito1minuto);
+   let digito2minuto=minutos%10;
+   aDigitos.push(digito2minuto);
+
+   let digito1segundo=parseInt(segundos/10);
+   aDigitos.push(digito1segundo);
+   let digito2segundo=segundos%10;
+   aDigitos.push(digito2segundo);
+
    var reloj = document.getElementById("reloj");
-   reloj.innerHTML=hora;
-   
+    
+   //recorremos el array de hijos de children cambiando el atributo src de cada imagen para poner el digito correspondiente
+   for (const image of reloj.children) {
+    if(image.getAttribute("alt")=="colon"){
+            image.setAttribute("src","webroot/imagesHour/colon.png");
+        }else{
+            image.setAttribute("src","webroot/imagesHour/"+aDigitos[contador]+".png");
+            contador++;
+        }
+   }
 }
 
 Reloj();
@@ -106,6 +160,7 @@ function calcularDireccionAleatoria(dimension) {
 }
 var direccion=calcularDireccionAleatoria(dimension);
 
+//compruebo si la posicion y dirección es válida solo rspecto al tamaño
 function comprobarPosicionValida(tablero,posicion,direccion,palabra) {
     //la direccion 0 es hacia arriba, las direcciones se recorren como un reloj
     aPalabra=Array(palabra);
